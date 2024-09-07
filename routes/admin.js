6e77acc75817e27f88bf0aff79e1b2dca9a5e4d6
quangtran789 <quangvutran61@gmail.com>
2024-09-07 16:49:05 +0700

    uploading

--- routes/admin.js
+++ routes/admin.js
@@ -45,6 +45,75 @@ adminRouter.post("/admin/delete-product", admin, async (req, res) => {
   }
 });
 
+//get all orders
+adminRouter.get('/admin/get-orders', admin, async (req, res)=>{
+  try {
+    const orders = await Order.find({});
+    res.json(orders);
+  } catch (e) {
+    res.status(500).json({error: e.message});
+  }
+});
 
+adminRouter.post('/admin/change-order-status', admin, async(req,res)=>{
+  try {
+    const { id, status } = req.body;
+    let order = await Order.findById(id);
+    order.status = status;
+    order = await order.save();
+    res.json(order);
+  } catch (e) {
+    res.status(500).json({error:e.message});
+  }
+});
+
+
+adminRouter.get("/admin/analytics", admin, async (req, res) => {
+  try {
+    const orders = await Order.find({});
+    let totalEarnings = 0;
+
+    for (let i = 0; i < orders.length; i++) {
+      for (let j = 0; j < orders[i].products.length; j++) {
+        totalEarnings +=
+          orders[i].products[j].quantity * orders[i].products[j].product.price;
+      }
+    }
+    //category wise order fetching
+    let hoatuoiearnings = await fetchCategoryWiseProduct('Hoa tươi');
+    let hoakhoearnings = await fetchCategoryWiseProduct('Hoa khô');
+    let hatgiongearnings = await fetchCategoryWiseProduct('Hạt giống');
+    let chauhoaearnings = await fetchCategoryWiseProduct('Chậu hoa');
+    let hoasukienearnings = await fetchCategoryWiseProduct('Hoa sự kiện');
+
+    let earnings  ={
+      totalEarnings,
+      hoatuoiearnings,
+      hoakhoearnings,
+      hatgiongearnings,
+      chauhoaearnings,
+      hoasukienearnings,
+    }
+    res.json(earnings);
+  } catch (e) {
+    res.status(500).json({error:e.message});
+  }
+});
+
+async function fetchCategoryWiseProduct(category) {
+  let earnings = 0;
+  let categoryOrders = await Order.find({
+    "products.product.category": category,
+  });
+
+  for (let i = 0; i < categoryOrders.length; i++) {
+    for (let j = 0; j < categoryOrders[i].products.length; j++) {
+      earnings +=
+        categoryOrders[i].products[j].quantity *
+        categoryOrders[i].products[j].product.price;
+    }
+  }
+  return earnings;
+}
 
 module.exports = adminRouter;
